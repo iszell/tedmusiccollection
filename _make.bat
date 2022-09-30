c7905676308cc441ed6e12a18cafcc90f580573e
Siz <siz@siz.hu>
2022-09-30 03:15:51 +0200

    Start freshly built song

--- _make.bat
+++ _make.bat
@@ -1,4 +1,6 @@
 gcc -o tmftool.exe tmftool.c
 call ka tmfplayer.asm
 call tass -o exorcist.prg exorcist.asm
-start exorcist.prg
+
+tmftool --init 0x2f01 --play 0x2f25 --timing pal2x --title "Exorcist" --author "David Whittaker" --released "1984 Mr. Micro Ltd." --player tmfplayer.prg exorcist_patched.prg exorcist.tmf.prg
+start exorcist.tmf.prg
